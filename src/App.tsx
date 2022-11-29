9b9d6778bcdfed3b9147a220754c588a121d3183
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2022-11-29 16:33:07 +0100

    feature: circle draw, undo, redo ready;

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+type TPoint = {
+    xPos: number,
+    yPos: number
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [circles, setCircles] = useState<TPoint[]>([]);
+    const [removedCircles, setRemovedCircles] = useState<TPoint[]>([]);
+
+    const createCircle = (xPos: number, yPos: number, idx: number): JSX.Element => {
+        return <div
+            className={'circle'}
+            key={`circle-${idx}`}
+            style={{
+                top: `${yPos}px`,
+                left: `${xPos}px`
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </div>;
+    };
+
+
+    function removeLastCircle(e: React.MouseEvent<HTMLButtonElement>) {
+        e.stopPropagation();
+        setRemovedCircles(curr => [...curr, circles[circles.length - 1]]);
+        setCircles([...circles.slice(0, circles.length - 1)]);
+    }
+
+    function restoreLastCircle(e: React.MouseEvent<HTMLButtonElement>) {
+        e.stopPropagation();
+        setCircles([...circles, removedCircles[removedCircles.length - 1]]);
+        setRemovedCircles([...removedCircles.slice(0, removedCircles.length - 1)]);
+    }
+
+    return (
+        <div
+            className="App"
+            onClick={e => setCircles([...circles, {xPos: e.clientX, yPos: e.clientY}])}
+        >
+            <div className={'buttonsArea'}>
+                <button
+                    className={'button'}
+                    onClick={e => removeLastCircle(e)}
+                    disabled={circles.length === 0}
+                >
+                        Undo
+                </button>
+                <button
+                    className={'button'}
+                    onClick={e => restoreLastCircle(e)}
+                    disabled={removedCircles.length === 0}
+                >
+                        Redo
+                </button>
+            </div>
+
+            {circles.map((circle, idx: number) => createCircle(circle.xPos, circle.yPos, idx))}
+        </div>
+    );
 }
 
 export default App;
